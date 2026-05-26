add6abe9afe2cd30a4b75478d6d5400c1a11610a
OriginalEthnix <dhruvwadhwa8661@gmail.com>
2026-05-27 03:48:03 +0530

    Fix PredictionTabs type mismatch

--- client/components/PredictionTabs.tsx
+++ client/components/PredictionTabs.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useMemo } from "react";
+import { CollegeData } from "./CollegeCard";
 
 export type TabType = "All" | "Safe" | "Target" | "Dream";
 
@@ -11,7 +12,7 @@ interface College {
 interface PredictionTabsProps {
   activeTab: TabType;
   onTabChange: (tab: TabType) => void;
-  colleges: College[];
+  colleges: CollegeData[];
 }
 
 const TABS: { key: TabType; label: string; activeClass: string }[] = [
